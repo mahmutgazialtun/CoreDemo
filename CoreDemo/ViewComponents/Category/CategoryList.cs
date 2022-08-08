4cd3cd9049c730e72140c3e1b960bb2758d261cb
Mahmut Gazi Altun <mahmutaltun535@gmail.com>
2022-08-08 17:41:42 +0300

    32.Ders sonu

--- CoreDemo/ViewComponents/Category/CategoryList.cs
+++ CoreDemo/ViewComponents/Category/CategoryList.cs
@@ -6,12 +6,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace CoreDemo.Controllers
+namespace CoreDemo.ViewComponents.Category
 {
-    public class Category : Controller
+    public class CategoryList: ViewComponent
     {
         CategoryManager cm = new CategoryManager(new EfCategoryRepository());
-        public IActionResult Index()
+        public IViewComponentResult Invoke()
         {
             var values = cm.GetList();
             return View(values);
---
+++ CoreDemo/ViewComponents/Commentslist.cs
@@ -0,0 +1,35 @@
+﻿using CoreDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.ViewComponents
+{
+    public class Commentslist:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var commentvalues = new List<UserComment>
+            {
+                new UserComment
+                {
+                    ID=1,
+                    Username="Gazi",
+                },
+                new UserComment
+                {
+                    ID=2,
+                    Username="Mesut"
+                },
+                new UserComment
+                {
+                    ID=3,
+                    Username="Merve"
+                }
+            };
+            return View(commentvalues);
+        }
+    }
+}
