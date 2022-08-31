e019417e7243edfaaa3ed918f032cb32c35be56d
Mahmut Gazi Altun <mahmutaltun535@gmail.com>
2022-08-31 17:55:01 +0300

    86.Ders Sonu

--- CoreDemo/Controllers/DashboardController.cs
+++ CoreDemo/Controllers/DashboardController.cs
@@ -10,13 +10,16 @@ namespace CoreDemo.Controllers
 {
     public class DashboardController : Controller
     {
-        [AllowAnonymous]
         public IActionResult Index()
         {
             Context c = new Context();
             ViewBag.v1 = c.Blogs.Count().ToString();
             ViewBag.v2 = c.Blogs.Where(x => x.WriterID == 1).Count();
             ViewBag.v3 = c.Categories.Count();
+            return View();
+        }
+        public IActionResult Deneme(int id)
+        {
             return View();
         }
     }
