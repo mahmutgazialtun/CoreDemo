17d2871cb2b7485a5b9fd23b954e21f2d6d0d1f2
Mahmut Gazi Altun <mahmutaltun535@gmail.com>
2022-09-06 13:19:07 +0300

    101. Ders Sonu

---
+++ CoreDemo/Areas/Admin/ViewComponents/Statictic/Statistic4.cs
@@ -0,0 +1,19 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Areas.Admin.ViewComponents.Statictic
+{
+    public class Statistic4:ViewComponent
+    {
+        Context c = new Context();
+        public IViewComponentResult Invoke()
+        {
+            
+            return View();
+        }
+    }
+}
